f8f3d8106aa40891fe58b3829ba25fadd50cffb2
Oswall Moena <oswmoena@gmail.com>
2024-07-22 21:03:56 -0400

    fix router v6

--- src/main.tsx
+++ src/main.tsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
             }
         ]
     },
-]);
+], { basename: "/app"});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
